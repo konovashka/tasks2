28a7006934772a84e62498d83be791dcdccf4cf8
konovashka <anton_konovalov_1@mail.ru>
2021-11-08 23:20:26 +0300

    Добавьте файлы проекта.

---
+++ tasks2/tasks2.cpp
@@ -0,0 +1,25 @@
+﻿// Проверка на палиндром
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n,a,ost,copy;
+		a = 0;
+	ost = 0;
+	scanf_s("%d", &n);
+	copy = n;
+	while (n>0)
+	{
+		ost = n % 10;
+		a = a * 10 + ost;
+		n = n / 10;
+	}
+	if (a==copy)
+	{
+		printf("palindrom");
+	}
+	else
+	{
+		printf("ne palindrom");
+	}
+}
